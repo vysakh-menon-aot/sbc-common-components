0c5c4e2d5bcddd3208262264ea98301ddedae9db
jeznorth <twenty15creative@gmail.com>
2019-07-26 13:42:47 -0700

    [entity813] Adding Title to Header Component (#19)

--- vue/sbc-common-components/src/components/SbcHeader.vue
+++ vue/sbc-common-components/src/components/SbcHeader.vue
@@ -1,18 +1,19 @@
 <template>
-    <header class="app-header" fixed app>
-        <div class="container">
-            <a href="/" class="navbar-brand">
-                <img class="brand-img" src="../assets/img/gov3_bc_logo.png"
-                     alt="Province of British Columbia Logo"
-                     title="Province of British Columbia"/>
-            </a>
-            <div class="app-header__actions">
-              <span v-if="authorized"><button class="v-btn v-btn--outline v-btn--depressed theme--light" @click="logout">Sign Out</button></span>
-              <!-- temporary have "sign in" button which also trigger logout for unit test before figure out session mock for test case. Can be removed later.-->
-              <span v-else><button class="v-btn v-btn--outline v-btn--depressed theme--light" @click="logout">Sign In</button></span>
-            </div>
-        </div>
-    </header>
+  <header class="app-header" fixed app>
+    <div class="container">
+      <a href="/" class="navbar-brand">
+        <img class="brand-img" src="../assets/img/gov3_bc_logo.png"
+          alt="Province of British Columbia Logo"
+          title="Province of British Columbia"/>
+      </a>
+      <span class="brand-title">BC Registries & Online Services</span>
+      <div class="app-header__actions">
+        <span v-if="authorized"><button class="v-btn v-btn--outline v-btn--depressed theme--light" @click="logout">Sign Out</button></span>
+        <!-- temporary have "sign in" button which also trigger logout for unit test before figure out session mock for test case. Can be removed later.-->
+        <span v-else><button class="v-btn v-btn--outline v-btn--depressed theme--light" @click="logout">Sign In</button></span>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
@@ -48,30 +49,35 @@ export default Vue.extend({
 </script>
 
 <style lang="stylus" scoped>
-    @import "../assets/styl/theme.styl"
+  @import "../assets/styl/theme.styl"
 
-    .app-header
-        position fixed
-        width 100%
-        color #fff
-        border-bottom 3px solid $BCgovGold5
-        background-color $BCgovBlue5
+  .app-header
+    position fixed
+    width 100%
+    color #fff
+    border-bottom 3px solid $BCgovGold5
+    background-color $BCgovBlue5
 
-        .container
-            display flex
-            align-items center
-            padding-top 0
-            padding-bottom 0
+    .container
+      display flex
+      align-items center
+      padding-top 0
+      padding-bottom 0
 
-    .app-header__actions
-       margin-left auto
+  .app-header__actions
+      margin-left auto
 
-      .v-btn
-        margin-right 0
-        color #ffffff
-        border-color #ffffff
+    .v-btn
+      margin-right 0
+      color #ffffff
+      border-color #ffffff
 
-    .brand-img
-        margin-top 0.3rem
+  .brand-img
+    margin-top 0.3rem
+    margin-right 1.25rem
 
+  .brand-title
+    margin-top 0.2rem
+    font-size 1.125rem
+    font-weight 300
 </style>
