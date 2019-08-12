9bf12cbce17bfd3038ad4072e340fe1051ba464b
kvangorp <46982021+kvangorp@users.noreply.github.com>
2019-08-12 16:57:26 -0700

    Setup vuetify a la carte (#30)
    * Configure vuetify to be a-la-carte
    
    * Add additonal vuetify components to vuetify.ts

--- vue/sbc-common-components/src/plugins/vuetify.ts
+++ vue/sbc-common-components/src/plugins/vuetify.ts
@@ -1,8 +1,17 @@
 import 'material-icons/iconfont/material-icons.css' // Ensure you are using css-loader
-import Vue from 'vue'
-import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue'
+import Vuetify, {
+  VAlert,
+  VContainer,
+  VIcon
+} from 'vuetify/lib'
 
 Vue.use(Vuetify, {
-  iconfont: 'md'
+  iconfont: 'md',
+  components: {
+    VAlert,
+    VContainer,
+    VIcon
+  }
 })
