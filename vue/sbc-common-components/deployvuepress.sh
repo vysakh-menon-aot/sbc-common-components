3708ce7cdf27c478db5dab30b70751169a6a91c8
kvangorp <46982021+kvangorp@users.noreply.github.com>
2019-08-14 15:16:35 -0700

    Update deployvuepress.sh to point to bcgov repo (#32)

--- vue/sbc-common-components/deployvuepress.sh
+++ vue/sbc-common-components/deployvuepress.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:saravankumarpa/sbc-common-components.git master:gh-pages
+git push -f git@github.com:bcgov/sbc-common-components.git master:gh-pages
 
 cd -
