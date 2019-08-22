e939b18c1995fe67388ad47ad7c826e6766f7bad
jeznorth <twenty15creative@gmail.com>
2019-08-22 09:07:48 -0700

    [entity1076] Implement Responsive Header

--- vue/sbc-common-components/src/components/SbcHeader.vue
+++ vue/sbc-common-components/src/components/SbcHeader.vue
@@ -1,14 +1,21 @@
 <template>
   <header class="app-header">
     <div class="container">
-      <a href="/" class="navbar-brand">
-        <img class="brand-img" src="../assets/img/gov3_bc_logo.png"
-             alt="Province of British Columbia Logo"
-             title="Province of British Columbia"/>
+      <a href="/" class="brand">
+        <picture>
+          <source media="(min-width: 601px)" 
+            srcset="../assets/img/gov_bc_logo_horiz.png">
+          <source media="(max-width: 600px)" 
+            srcset="../assets/img/gov_bc_logo_vert.png">
+          <img class="brand__image" 
+            src="../assets/img/gov_bc_logo_vert.png" 
+            alt="Government of British Columbia Logo" 
+            title="Government of British Columbia">
+        </picture>
+        <span class="brand__title">BC Registries <span class="brand__title--wrap">& Online Services</span></span>
       </a>
-      <span class="brand-title">BC Registries & Online Services</span>
       <div class="app-header__actions">
-        <span v-if="authorized"><button class="v-btn v-btn--outline v-btn--depressed theme--light" @click="logout">Sign Out</button></span>
+        <button class="v-btn v-btn--outline v-btn--depressed theme--light" v-if="authorized" @click="logout">Sign Out</button>
       </div>
     </div>
   </header>
@@ -61,23 +68,44 @@ export default Vue.extend({
     .container
       display flex
       align-items center
+      height 100%
       padding-top 0
       padding-bottom 0
 
   .app-header__actions
-    margin-left auto
+      margin-left auto
 
-  .v-btn
-    margin-right 0
-    color #ffffff
-    border-color #ffffff
+    .v-btn
+      margin-right 0
+      color #ffffff
+      border-color #ffffff
 
-  .brand-img
-    margin-top 0.3rem
-    margin-right 1.25rem
+  .brand 
+    display flex
+    align-items center
+    padding-right 1rem
+    text-decoration none 
+    color inherit
 
-  .brand-title
-    margin-top 0.2rem
+  .brand__image
+    display block
+    margin-right 1.5rem
+    margin-left -0.1rem
+    max-height 70px
+
+  .brand__title
     font-size 1.125rem
     font-weight 400
+
+  @media (max-width 600px)
+    .brand__image
+      margin-right 0.75rem
+      margin-left -0.15rem
+
+    .brand__title
+      font-size 1rem
+      line-height 1.25rem
+
+    .brand__title--wrap 
+      display block
 </style>
