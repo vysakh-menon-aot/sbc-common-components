94085cf34e30e57f3c9d25a67a3aa51c6897530b
Andrew Sutherland <sutherlanda@gmail.com>
2020-01-29 13:09:07 -0800

    Fix bug issue with status service

--- vue/sbc-common-components/src/services/status.services.ts
+++ vue/sbc-common-components/src/services/status.services.ts
@@ -2,10 +2,8 @@ import Axios, { AxiosResponse } from 'axios'
 import { ServiceStatus } from '../models'
 import ConfigHelper from '../util/config-helper'
 
-const statusUrl = ConfigHelper.getStatusAPIUrl()
-
 export default class StatusService {
   static getServiceStatus (serviceName: string): Promise<AxiosResponse<ServiceStatus>> {
-    return Axios.get(`${statusUrl}/status/${serviceName}`)
+    return Axios.get(`${ConfigHelper.getStatusAPIUrl()}/status/${serviceName}`)
   }
 }
