015f7716d2ddd45efd105760652d472cef2f940d
Nitheesh T Ganesh <60233274+nitheesh-aot@users.noreply.github.com>
2020-03-26 16:56:25 -0700

    conditionally avoided notification count call for bcros users (#137)
    * conditionally avoided notification count call for bcros&idir users

--- vue/sbc-common-components/src/store/modules/account.ts
+++ vue/sbc-common-components/src/store/modules/account.ts
@@ -72,7 +72,9 @@ export default class AccountModule extends VuexModule {
     if (response && response.data) {
       const orgs = response.data.filter(userSettings => (userSettings.type === 'ACCOUNT'))
       this.context.commit('setCurrentAccount', currentAccountId ? orgs.find(org => String(org.id) === currentAccountId) : orgs[0])
-      await this.context.dispatch('fetchPendingApprovalCount')
+      if (this.currentUser?.loginSource === 'BCSC') {
+        await this.context.dispatch('fetchPendingApprovalCount')
+      }
       return orgs
     }
     return []
